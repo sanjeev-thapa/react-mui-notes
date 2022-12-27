f0fbf89c04d80d531858225358268f3e477a45bd
sanjeev.thapa <stedwardson@gmail.com>
2022-12-28 03:49:45 +0545

    Setup React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      Hello World
+      <Router>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
     </>
   );
 }

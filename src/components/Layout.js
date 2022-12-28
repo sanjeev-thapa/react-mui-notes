494e500996a6df5e08fa0ca950178360ee70af8b
sanjeev.thapa <stedwardson@gmail.com>
2022-12-28 06:13:59 +0545

    Add AppBar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,45 @@
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Badge from "@mui/material/Badge";
+
 const Layout = ({ children }) => {
     return (
         <>
-            { children }
+            <AppBar
+                elevation={0}
+            >
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        size="large"
+                        edge="start"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    <Typography sx={{flexGrow: 1}}>
+                        MUI Notes App
+                    </Typography>
+
+                    <Badge
+                        variant="dot"
+                        overlap="circular"
+                        anchorOrigin={{vertical: "bottom", horizontal: "right"}}
+                        color="warning"
+                    >
+                        <Avatar>ST</Avatar>
+                    </Badge>
+                </Toolbar>
+            </AppBar>
+
+            <Box>
+                {children}
+            </Box>
         </>
     );
 }

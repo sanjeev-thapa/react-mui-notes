5183dfd98f8ce40f74a8a24134e3dd75c0e439d5
sanjeev.thapa <stedwardson@gmail.com>
2023-02-05 08:42:50 +0545

    Add ToastContainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Create from "./pages/Create";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           </Routes>
         </Layout>
       </Router>
+
+      <ToastContainer autoClose={10000} newestOnTop={true} />
     </>
   );
 }

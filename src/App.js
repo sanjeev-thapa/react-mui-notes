7211cade86553a99c295438219c0dd64775d528a
sanjeev.thapa <stedwardson@gmail.com>
2023-02-09 13:07:51 +0545

    Create Custom Theme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Create from "./pages/Create";
 import Notes from "./pages/Notes";
-import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import { createTheme, ThemeProvider } from "@mui/material";
+import "@fontsource/quicksand/400.css";
+import "@fontsource/quicksand/500.css";
+import "@fontsource/quicksand/600.css";
+import "@fontsource/quicksand/700.css";
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3867d0"
+      }
+    },
+    typography: {
+      fontFamily: "'Quicksand', sans-serif;",
+      fontWeightLight: 400,
+      fontWeightRegular: 500,
+      fontWeightMedium: 600,
+      fontWeightBold: 700
+    }
+  });
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router>
         <Layout>
           <Routes>
@@ -18,7 +39,7 @@ function App() {
       </Router>
 
       <ToastContainer autoClose={10000} newestOnTop={true} />
-    </>
+    </ThemeProvider>
   );
 }
 
